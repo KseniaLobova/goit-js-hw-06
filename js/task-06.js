8b8dc9b1bee04e1219aaf7085081db375ef1dba8
KseniaLobova <128805342+KseniaLobova@users.noreply.github.com>
2023-07-26 11:58:44 +0200

    finish

---
+++ js/task-06.js
@@ -0,0 +1,16 @@
+const blurInput = document.querySelector('#validation-input')
+// console.log(blurInput)
+blurInput.addEventListener('blur', onBlureInput);
+
+
+function onBlureInput(evt) {
+    
+    if (evt.currentTarget.value.length === Number(blurInput.dataset.length) ) {
+     
+        blurInput.classList.add('valid')
+    } else {
+        blurInput.classList.remove('valid')
+        blurInput.classList.add('invalid')
+        
+    }
+}
